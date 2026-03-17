ad9dcfd388b6adb040381dcd5d939ba7a1add25b
wangxl <wangty334752@gmail.com>
2026-03-17 21:55:57 +0800

    feat: 设计微博登录（todo）

--- atlas-auth/src/main/java/com/bubua12/atlas/auth/controller/OAuth2Controller.java
+++ atlas-auth/src/main/java/com/bubua12/atlas/auth/controller/OAuth2Controller.java
@@ -1,13 +1,18 @@
 package com.bubua12.atlas.auth.controller;
 
+import cn.hutool.http.HttpUtil;
 import com.bubua12.atlas.api.system.dto.SysUserDTO;
 import com.bubua12.atlas.auth.feign.AtlasSystemFeign;
 import com.bubua12.atlas.common.core.result.CommonResult;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 处理社交登录请求  todo 对接微博登录
@@ -22,19 +27,37 @@ public class OAuth2Controller {
     @Resource
     private AtlasSystemFeign atlasSystemFeign;
 
+    @Value("${atlas.weibo.client-id}")
+    private String weiboClientId;
+
+    @Value("${atlas.weibo.client-secret}")
+    private String weiboClientSecret;
+
+    @Value("${atlas.weibo.redirect-uri}")
+    private String weiboRedirectUri;
+
 
     @GetMapping("/oauth2/weibo/success")
     public String weibo(@RequestParam("code") String code) {
-
-        // fixme 发送http请求的工具类
         // 1、根据code换取 accessToken
+        String postUrl = "https://api.weibo.com/oauth2/access_token";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("client_id", weiboClientId);
+        params.put("client_secret", weiboClientSecret);
+        params.put("grant_type", "authorization_code");
+        params.put("code", code);
+        params.put("redirect_uri", weiboRedirectUri);
+
+        // 发送请求
+        String response = HttpUtil.post(postUrl, params);
+        System.out.println(response);
 
         String accessToken = "";
 
         // 知道当前是哪个社交用户
         // 1、登录或者注册这个社交用户：当前用户如果是第一次进网站，自动注册进来 -- 为当前社交用户生成一个用户账号 -- 建立设计用户账号与系统用户的关联
         // 2、
-
 
 
         // 2、登录成功就跳回首页
